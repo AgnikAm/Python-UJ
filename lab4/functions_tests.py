db546a096a587242781c0bbce920e283cf95cd61
AgnikAm <KingaAleksandraKlima@gmail.com>
2023-11-10 15:53:00 +0100

    Finished

--- lab4/functions_tests.py
+++ lab4/functions_tests.py
@@ -82,28 +82,5 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(flatten([1,(2,3),[],[4,(5,6,7)],8,[9]]), [1, 2, 3, 4, 5, 6, 7, 8, 9])
         self.assertEqual(flatten([8, (2, 9), [], [1, (3, 2, 3)], 0, [9]]), [8, 2, 9, 1, 3, 2, 3, 0, 9])
 
-
-
-    '''def test_duplicated_elements(self):
-        a = [0, 3, 6, 9]
-        b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.assertEqual(duplicated_elements(a, b), [3, 6, 9])
-    
-    def test_common_elements(self):
-        a = [0, 3, 6, 9]
-        b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.assertEqual(common_elements(a, b), [0, 3, 6, 9, 1, 2, 4, 5, 7, 8])
-    
-    def test_sum_sequence(self):
-        seq = [[],[4],(1,2),[3,4],(5,6,7)]
-        self.assertEqual(sum_sequence(seq), [0,4,3,7,18])
-
-    def test_roman2int(self):
-        self.assertEqual(roman2int("VIII"), 8)
-        self.assertEqual(roman2int("CXCIX"), 199)
-        self.assertEqual(roman2int("CCCLIV"), 354)
-        self.assertEqual(roman2int("MMMDCCXXIV"), 3724)
-'''
-
 if __name__ == '__main__':
     unittest.main()
