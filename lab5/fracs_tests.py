fd000ff89f494540249778749be72b8c68e36ed1
AgnikAm <KingaAleksandraKlima@gmail.com>
2023-11-18 21:00:19 +0100

    Change error in assertRaises

--- lab5/fracs_tests.py
+++ lab5/fracs_tests.py
@@ -9,7 +9,7 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(add_frac([1, 2], [1, 3]), [5, 6])
         self.assertEqual(add_frac([-3, 4], [-1, 4]), [-16, 16])
         self.assertEqual(add_frac([0, 2], [1, 3]), [2, 6])
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ZeroDivisionError):
             add_frac([1, 0], [1, 3])
             add_frac([1, 2], [1, 0])
 
@@ -17,7 +17,7 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(sub_frac([1, 2], [1, 3]), [1, 6])
         self.assertEqual(sub_frac([-3, 4], [-1, 4]), [-8, 16])
         self.assertEqual(sub_frac([0, 2], [1, 3]), [-2, 6])
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ZeroDivisionError):
             sub_frac([1, 0], [1, 3])
             sub_frac([1, 2], [1, 0])
 
@@ -26,7 +26,7 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(mul_frac([-3, 4], [-1, 4]), [3, 16])
         self.assertEqual(mul_frac([0, 2], [1, 3]), [0, 6])
         self.assertEqual(mul_frac([1, 4], [-1, 4]), [-1, 16])
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ZeroDivisionError):
             mul_frac([1, 0], [1, 3])
             mul_frac([1, 2], [1, 0])
 
@@ -35,7 +35,7 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(div_frac([-3, 4], [-1, 4]), [-12, -4])
         self.assertEqual(div_frac([0, 2], [1, 3]), [0, 2])
         self.assertEqual(div_frac([1, 4], [-1, 4]), [4, -4])
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ZeroDivisionError):
             div_frac([1, 0], [1, 3])
             div_frac([1, 2], [1, 0])
             div_frac([1, 2], [0, 2])
@@ -46,7 +46,7 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(is_positive([0, 2]), "The number is zero")
         self.assertEqual(is_positive([1, -4]), False)
         self.assertEqual(is_positive([-1, -4]), True)
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ZeroDivisionError):
             is_positive([1, 0])
 
     def test_is_zero(self):
@@ -54,7 +54,7 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(is_zero([-3, 4]), False)
         self.assertEqual(is_zero([0, 2]), True)
         self.assertEqual(is_zero([1, -4]), False)
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ZeroDivisionError):
             is_zero([1, 0])
 
     def test_cmp_frac(self):
@@ -64,7 +64,7 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(cmp_frac([3, -4], [1, 2]), -1)
         self.assertEqual(cmp_frac([1, 2], [3, -4]), 1)
         self.assertEqual(cmp_frac([-4, -1], [-5, -1]), -1)
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ZeroDivisionError):
             cmp_frac([1, 0], [1, 3])
             cmp_frac([1, 2], [1, 0])
 
@@ -73,7 +73,7 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(frac2float([-3, 4]), -0.75)
         self.assertEqual(frac2float([0, 2]), 0)
         self.assertEqual(frac2float([1, -4]), -0.25)
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ZeroDivisionError):
             frac2float([1, 0])
 
 
