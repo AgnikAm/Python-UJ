740e7acf097de2c2cb84b3caf20abca26d84bd0b
AgnikAm <KingaAleksandraKlima@gmail.com>
2023-11-24 15:31:15 +0100

    Create Rectangle class and implement its methods

---
+++ lab6/rectangles.py
@@ -0,0 +1,31 @@
+from points import Point
+
+class Rectangle:
+
+    def __init__(self, x1, y1, x2, y2):
+        self.pt1 = Point(x1, y1)
+        self.pt2 = Point(x2, y2)
+
+    def __str__(self):
+        return f"[({self.pt1.x}, {self.pt1.y}), ({self.pt2.x}, {self.pt2.y})]"
+
+    def __repr__(self):
+        return f"Rectangle({self.pt1.x}, {self.pt1.y}, {self.pt2.x}, {self.pt2.y})"
+
+    def __eq__(self, other):
+        return self.pt1 == other.pt1 and self.pt2 == other.pt2
+
+    def __ne__(self, other):
+        return not self == other
+
+    def center(self):
+        return((self.pt1.x + self.pt2.x) / 2, (self.pt1.y + self.pt2.y) / 2)
+
+    def area(self):
+        return abs(self.pt1.x - self.pt2.x) * abs(self.pt1.y - self.pt2.y)
+
+    def move(self, x, y):  # przesunięcie o (x, y)
+        self.pt1.x += x
+        self.pt2.x += x
+        self.pt1.y += y
+        self.pt2.y += y
